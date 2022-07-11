1741de2b2174312eb89556b304eb9b7221576a4f
russe433 <russe433@msu.edu>
2022-07-11 19:49:43 -0400

    updated index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,20 @@
 const User = require('./User');
+const Search = require('./Search');
+const Comments = require('./Comments');
+User.hasMany(Search, {
+    foreignKey: 'user_id'
+});
 
-module.exports = { User };
+Search.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+//user belongs to comment
+Comments.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+})
+//comment belongs to search
+Comment.belongsTo(Search, )
+
+module.exports = { User, Search, Comments };
