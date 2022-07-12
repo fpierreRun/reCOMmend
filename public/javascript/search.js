fb4e03c23c01d90c4ecd582c14aaa16a1ea79903
PickleRick <bmkersey@gmail.com>
2022-07-12 00:32:15 -0400

    added api constant to search handler

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,8 +1,10 @@
 async function searchFormSubmitHandler(event) {
     event.preventDefault();
 
+    const key ='439107-Reccomen-KF6ZAIUS'
     const query = document.querySelector('#query-box').value.trim();
     const type = document.querySelector('#type-box').value.trim();
+    
     const response = await fetch('https://tastedive.com/api/similar?q=' + query + '&type=' + type + '&info=1&k='+ key)
          // check the response status
         if (response.ok) {
